328b471a815de678dc502627ff6d019279e45353
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-08-01 22:26:53 -0700

    Responsive features added 2.0

---
+++ main.js
@@ -0,0 +1,7 @@
+const burger = document.getElementsByClassName('burger');
+const container = document.getElementsByClassName('container');
+const nav = document.querySelector('nav');
+
+function navAppear (){
+    burger
+};
