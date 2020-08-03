d551a8046c8ae31811e01ff9fed05ae7e87adc0e
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-08-02 17:33:30 -0700

    Burger event listeners 1.0

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-const burger = document.getElementsByClassName('burger');
-const container = document.getElementsByClassName('container');
-const nav = document.querySelector('nav');
+const burger = document.getElementById('burger');
+const container = document.getElementById('container');
+const nav = document.getElementById('nav-bar');
+const exit = document.getElementById('exit');
 
 function navAppear (){
-    burger
+    burger.style.display = "none";
+    // container.style.display = "none";
+    exit.style.display = "block";
+    nav.style.display = "flex";
+    // nav.style.position = "relative";
+    console.log('click undido');
 };
+
+function navDisappear (){
+    burger.style.display = "block";
+    // container.style.display = "none";
+    exit.style.display = "none";
+    nav.style.display = "none";
+    console.log('click undido');
+};
+
+burger.onclick = navAppear;
+exit.onclick = navDisappear;
