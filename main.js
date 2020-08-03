17eeab7aa0ab4ee2786ceaa8e12f26f47663eb9b
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-08-03 13:55:08 -0700

    responsive navBar fixed 1.2

--- main.js
+++ main.js
@@ -3,33 +3,40 @@ const container = document.getElementById('container');
 const nav = document.getElementById('nav-bar');
 const exit = document.getElementById('exit');
 
+// if(window.innerWidth > 600){
+//     exit.style.display = "none";
+// }
+
 function navAppear (){
     burger.style.display = "none";
     // container.style.display = "none";
     exit.style.display = "block";
     nav.style.display = "flex";
+    nav.style.flexDirection = "column";
     // nav.style.position = "relative";
     console.log('click undido');
 };
 
 function navDisappear (){
-    burger.style.display = "block";
+    burger.style.display = "";
     // container.style.display = "none";
-    exit.style.display = "none";
-    nav.style.display = "none";
+    exit.style.display = "";
+    nav.style.display = "";
     console.log('click undido');
 };
 
 function resize() {
-    if (window.innerWidth >= 600){
+    if (window.innerWidth >= 601){
         nav.style.display = "flex";
         nav.style.flexDirection = "row"; 
-        exit.style.display = "none";     
+        exit.style.display = "none";
+        // console.log(window.innerWidth);     
     } else{
         nav.style.display = "none";
         nav.style.flexDirection = "column"; 
         exit.style.display = "none";
         burger.style.display = "block"; 
+        // console.log(window.innerWidth);  
     }
 }
 
